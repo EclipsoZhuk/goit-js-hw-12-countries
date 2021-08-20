d3093789254b3cb6ee4eaa4e1732484b68c19dfe
Vlad Gonchar <vladik.gonchar2015@gmail.com>
2021-08-21 00:05:03 +0300

    Дз 12

--- src/index.js
+++ src/index.js
@@ -1,1 +1,54 @@
 import './sass/main.scss';
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/desktop/dist/PNotifyDesktop';
+import '@pnotify/core/dist/BrightTheme.css';
+
+import { error } from '@pnotify/core';
+import refs from './js/refs';
+import countryCardTlt from './templates/country-card.hbs';
+import listCountriesTpl from './templates/list-countries.hbs';
+import fetchCountry from './js/fetchCountries';
+import debounce from 'lodash.debounce';
+
+refs.input.addEventListener('input', debounce(onSearch, 500));
+
+function onSearch() {
+    onInputClear();
+    const searchQuery = refs.input.value.trim();
+    fetchCountry(searchQuery)
+        .then(country => {
+            if (country.length > 10) {
+                error({
+                    text: 'Too many matches found. Please enter a more specific query!',
+                });
+            } else if (country.status === 404) {
+                error({
+                    text: 'No country has been found. Please enter a more specific query!',
+                });
+            } else if (country.length === 1) {
+                onRenderCountryCard(country);
+            } else if (country.length <= 10) {
+                onRenderListCountries(country);
+            }
+        })
+        .catch(onFetchError);
+}
+
+function onRenderCountryCard(country) {
+    const markup = countryCardTlt(country);
+    refs.infoBox.innerHTML = markup;
+}
+
+function onRenderListCountries(country) {
+    const listMarkup = listCountriesTpl(country);
+    refs.countries.insertAdjacentHTML('beforeend', listMarkup);
+}
+
+function onInputClear() {
+    refs.infoBox.innerHTML = '';
+    refs.countries.innerHTML = '';
+}
+
+function onFetchError(Error) {
+    Error;
+}
